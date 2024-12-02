6d0575e1eb8ab9e8ca133df8f0e9849590312a78
alhoussine04 <128273143+alhoussine04@users.noreply.github.com>
2024-12-02 01:56:09 -0500

    Update measurementform.h

--- measurementform.h
+++ measurementform.h
@@ -12,21 +12,39 @@
 #include <vector>
 #include  <QString>
 #include "measurementManager.h"
+#include "healthmetricVisualization.h"
+
 class MeasurementForm : public QWidget
 {
     Q_OBJECT
 public:
     MeasurementForm(QWidget *parent = nullptr);
     void handleNext();
+    void simulateDeviceContact();
+
+
 signals:
+    void measurementCompleted();
     void nextClicked();
     void exitClicked();
+
 private:
     QVBoxLayout *mainLayout;
+    HealthMetricVisualization* metricsVisualization;
     int stepNumber;
+    bool noMeasure;
+    bool isDeviceContacting;
     QLabel* picture;
    std::vector<QString> imagePaths;
    MeasurementManager* measurementManager;
+   QLabel *contactStateLabel;
+   QPushButton* viewVisualizationBtn;
+   QPushButton *nextBtn;
+   QPushButton *exitBtn;
+
+   void showMeasurementVisualization();
+   void startDeviceContact();
+   void endDeviceContact();
 
 };
 
