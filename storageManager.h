3351b64f7426453c8b617d27a4b30d78ace7fed5
alhoussine04 <128273143+alhoussine04@users.noreply.github.com>
2024-12-03 15:58:55 -0500

    Update storageManager.h

--- storageManager.h
+++ storageManager.h
@@ -5,18 +5,25 @@
 #include <QFile>
 #include <QJsonDocument>
 #include <QDebug>
+#include <QDir>
 #include "userprofile.h"
 
 class StorageManager {
 public:
+    static StorageManager& getInstance() { static StorageManager instance; return instance; }
     void saveMeasurement(const QJsonObject &measurement, const QString &fileName);
     void saveData(const QJsonObject &data, const QString &fileName);
     void saveUserProfile(const UserProfile &user, const QString &fileName);
 
-
     QJsonObject loadMeasurement(const QString &fileName) const;
     QJsonObject loadData(const QString &fileName) const;
     UserProfile loadUserProfile(const QString &fileName) const;
+    QList<QJsonObject> loadAllMeasurements();
+
+private:
+    StorageManager() {} 
+    StorageManager(const StorageManager&) = delete;
+    StorageManager& operator=(const StorageManager&) = delete;
 };
 
 #endif // STORAGEMANAGER_H
